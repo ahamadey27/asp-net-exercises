76b1987904d1b862a78ca67aa54de6a332ba3a68
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-20 16:37:03 -0400

    Add game data initialization and update launch settings to disable browser launch

--- GameStore/GameStore.Api/Program.cs
+++ GameStore/GameStore.Api/Program.cs
@@ -1,8 +1,15 @@
+using GameStore.Api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
 
+List <GameDto> games = [
+    new (1, "The Legend of Zelda: Breath of the Wild", "Adventure", 59.99M, new DateOnly(2017, 3, 3)),
+    new (2, "God of War", "Action", 49.99M, new DateOnly(2018, 4, 20)),
+    new (3, "Minecraft", "Sandbox", 26.95M, new DateOnly(2011, 11, 18))
+];
+
 app.MapGet("/", () => "Hello World!");
-
-
 
 app.Run();
