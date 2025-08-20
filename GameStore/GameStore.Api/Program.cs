c492f16647246113f85cb069067bc847488507ab
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-20 16:45:51 -0400

    Add game retrieval endpoints and update game list initialization

--- GameStore/GameStore.Api/Program.cs
+++ GameStore/GameStore.Api/Program.cs
@@ -4,12 +4,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-List <GameDto> games = [
+List<GameDto> games = [
     new (1, "The Legend of Zelda: Breath of the Wild", "Adventure", 59.99M, new DateOnly(2017, 3, 3)),
     new (2, "God of War", "Action", 49.99M, new DateOnly(2018, 4, 20)),
     new (3, "Minecraft", "Sandbox", 26.95M, new DateOnly(2011, 11, 18))
 ];
 
-app.MapGet("/", () => "Hello World!");
+//GET /games
+app.MapGet("games", () => games);
+
+//GET /games/1
+app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id)); ;
 
 app.Run();
--- GameStore/GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore/GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7057b7ccee623eead177b08148e8132d64a4634a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76b1987904d1b862a78ca67aa54de6a332ba3a68")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
