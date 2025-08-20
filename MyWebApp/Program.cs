a6ba896b5a0d0ad6da52fb4b018f463507eeeb82
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-20 13:50:55 -0400

    Exercise - Use built-in middleware

--- MyWebApp/Program.cs
+++ MyWebApp/Program.cs
@@ -1,44 +1,15 @@
-//By default, this method configures Kestrel as the web server. Kestrel is a cross-platform web server for ASP.NET Core designed for high performance
+
+using Microsoft.AspNetCore.Rewrite; //added automatically after using UseRewriter middleware 
+
 var builder = WebApplication.CreateBuilder(args); //sets up we host
 var app = builder.Build();
 
+//This code adds a URL rewriter middleware component that redirects requests from /history to /about. The AddRedirect() method takes two parameters: a regular expression pattern to match the request path, and the replacement path to redirect to.
+app.UseRewriter(new RewriteOptions().AddRedirect("history", "about"));
 
-/*
-. WebApplication.CreateBuilder(args) creates a new WebApplicationBuilder object.
-. builder.Build() creates a new WebApplication object.
-. The first app.Run() defines a delegate that takes a HttpContext object and returns a Task. The delegate writes "Hello world!" to the response.
-. The second app.Run() starts the app.*/
-//delegates added with .Run() are ALWAYS terminal...meaning they are the last middleware to run and only expect http object as a parameter 
-
-/*
-. app.Use() defines a middleware component that:
-. Writes "Hello from middleware 1. Passing to the next middleware!" to the response.
-. Passes the request to the next middleware component in the pipeline and waits for it to complete with await next.Invoke().
-. After the next component in the pipeline completes, it writes "Hello from middleware 1 again!"
-. The first app.Run() defines a middleware component that writes "Hello from middleware 2!" to the response.
-. The second app.Run() starts the app.
-
-*/
-
-app.Use(async (context, next) =>
-{
-    await context.Response.WriteAsync("Hello from middleware 1. Passing to the next middleware!\r\n");
-
-    // Call the next middleware in the pipeline
-    await next.Invoke();
-
-    await context.Response.WriteAsync("Hello from middleware 1 again!\r\n");
-});
-
-app.Run(async context =>
-{
-    await context.Response.WriteAsync("Hello from middleware 2!\r\n");
-});
-
+//called endpoint routing 
+app.MapGet("/", () => "Welcome to Contoso!");
+app.MapGet("/about", () => "Contoso was founded in 2000");
 //this starts the app
 app.Run();
-
-
-//If the request matches a defined route, the corresponding endpoint is executed. In this case, the app.MapGet("/", () => "Hello World!") endpoint handles requests to the root URL and returns the string 
-//app.MapGet("/", () => "Hello, Alex Hamadey");
 
--- MyWebApp/obj/Debug/net8.0/MyWebApp.AssemblyInfo.cs
+++ MyWebApp/obj/Debug/net8.0/MyWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd0c26d009c27fca5b0f0ff99abea87da239a629")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93f0b202fd8729228d40c4e102ded5819fe9e159")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
