2fdc2f548a9372a3716baab847b612ceb1b1e795
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-23 15:06:17 -0700

    Add TollRecordsController with basic actions and logging

--- TollAppWebApp/TollAppWebApp/obj/Debug/net8.0/TollAppWebApp.AssemblyInfo.cs
+++ TollAppWebApp/TollAppWebApp/obj/Debug/net8.0/TollAppWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TollAppWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71ae4bfa3f75bb83093025864a16b218ccd361d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34426ea7c4baa9a3308d9a74c48c68199121ab72")]
 [assembly: System.Reflection.AssemblyProductAttribute("TollAppWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TollAppWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
