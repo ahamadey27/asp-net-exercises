5089970649bdeb80d7fb975d2c767beb63bff31c
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-08-23 13:25:14 -0700

    DB Migration complete

--- TollAppWebApp/TollAppWebApp/Program.cs
+++ TollAppWebApp/TollAppWebApp/Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
@@ -51,3 +57,10 @@ record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 }
 
 record MessageRequest(string Text);
+
+class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+}
